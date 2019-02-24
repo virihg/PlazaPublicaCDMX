19ae9b6e7310714888da1df2d7128c24bd4656a3
Ali <ali@Alis-iMac.local>
2019-02-24 15:59:56 -0600

    registering content-block

---
+++ app/cells/decidim/participatory_processes/content_blocks/past_processes_settings_form_cell.rb
@@ -0,0 +1,20 @@
+
+# frozen_string_literal: true
+
+module Decidim
+  module ParticipatoryProcesses
+    module ContentBlocks
+      class PastProcessesSettingsFormCell < Decidim::ViewModel
+        alias form model
+
+        def content_block
+          options[:content_block]
+        end
+
+        def label
+          I18n.t("decidim.participatory_processes.admin.content_blocks.past_processes.max_results")
+        end
+      end
+    end
+  end
+end
