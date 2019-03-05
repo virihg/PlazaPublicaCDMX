5848576afd7725a743daef8122538d5067ab975c
Ali <ponentesincausa@gmail.com>
2019-03-04 20:38:47 -0600

    fixed helper bug

--- app/helpers/decidim/contextual_help_helper.rb
+++ app/helpers/decidim/contextual_help_helper.rb
@@ -3,6 +3,10 @@
 module Decidim
   # Helper to print layout elements for the different help elements available on the pages.
   module ContextualHelpHelper
+    def floating_help(id, &block)
+      render partial: "decidim/shared/floating_help", locals: { content: capture(&block), id: id }
+    end
+
     def non_floating_help(id, &block)
       render partial: "decidim/shared/non_floating_help", locals: { content: capture(&block), id: id }
     end
