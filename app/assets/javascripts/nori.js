317af34c69383e1a6e1eb8bf5a854577d5a47778
Jorge Noricumbo <nori@hacemoscodigo.com>
2019-10-17 20:11:47 -0500

    Actualiza javascript

--- app/assets/javascripts/nori.js
+++ app/assets/javascripts/nori.js
@@ -1,15 +1,3 @@
-console.log('hey nori 2');
-
-// jQuery(document).ready(function($){
-//     $cf = $('#mobile_phone_mobile_phone_number');
-//     $cf.blur(function(e){
-//         phone = $(this).val();
-//         match = phone.match(/([+]?\d{1,2}[.-\s]?)?(\d{3}[.-]?){2}\d{4}/g); 
-//         if ( match )
-//         {
-//             alert('Escribe 10 dígitos.');
-//             $('#mobile_phone_mobile_phone_number').val('');
-//             $('#mobile_phone_mobile_phone_number').focus();
-//         }
-//     });
-// });
+jQuery(document).ready(function($){
+    console.log('hey nori 2');
+});
